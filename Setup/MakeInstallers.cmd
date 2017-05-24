f43ddb01b23b3242a41510619bf42047a8750afe
EbenZhang <zhangyingneng@gmail.com>
2017-05-24 22:33:55 +0800

    Increase version number

--- Setup/MakeInstallers.cmd
+++ Setup/MakeInstallers.cmd
@@ -6,8 +6,8 @@ rem
 rem Update this version number with every release
 rem
 setlocal
-set version=2.49.99.12
-set numericVersion=2.49.99.12
+set version=2.50.99.13
+set numericVersion=2.50.99.13
 if not "%APPVEYOR_BUILD_VERSION%"=="" (
     set version=%APPVEYOR_BUILD_VERSION%
     set numericVersion=%APPVEYOR_BUILD_VERSION%
