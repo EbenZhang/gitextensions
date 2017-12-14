9eb775e621159d51dde798bc1089edf8c9909cde
Janusz Białobrzewski <jbialobr@o2.pl>
2017-12-14 09:53:55 +0100

    Revert a breaking change introduced without any explanation.
    (cherry picked from commit 4a3d0d704c3c1caa477a67bdd2aaf870465c989a)

--- GitCommands/Git/GitModule.cs
+++ GitCommands/Git/GitModule.cs
@@ -3111,7 +3111,6 @@ namespace GitCommands
             return messages.Select(cm =>
                 {
                     int idx = cm.IndexOf("\n");
-                    if (idx <= 0) return null;
                     string encodingName = cm.Substring(0, idx);
                     cm = cm.Substring(idx + 1, cm.Length - idx - 1);
                     cm = ReEncodeCommitMessage(cm, encodingName);
